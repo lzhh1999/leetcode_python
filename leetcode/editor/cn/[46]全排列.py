7635c18fc5b4df87a54b2ed5c5941d8d0df95319
lzhhuo <1007320404@qq.com>
2020-05-01 23:33:11 +0800

    树

---
+++ leetcode/editor/cn/[46]全排列.py
@@ -0,0 +1,29 @@
+# 给定一个 没有重复 数字的序列，返回其所有可能的全排列。 
+# 
+#  示例: 
+# 
+#  输入: [1,2,3]
+# 输出:
+# [
+#   [1,2,3],
+#   [1,3,2],
+#   [2,1,3],
+#   [2,3,1],
+#   [3,1,2],
+#   [3,2,1]
+# ] 
+#  Related Topics 回溯算法
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+import itertools
+
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return list(itertools.permutations(nums))
+# leetcode submit region end(Prohibit modification and deletion)
